60a677d1e3af9afe4c2c9153bc367ad831a11f70
JANDERSON BORGES DE LIMA <JANDERSON9261@DIGITALCOLLEGE.LOCAL>
2023-12-04 20:49:52 -0300

    código aula tira duvidas

---
+++ script.js
@@ -0,0 +1,89 @@
+// function nomeDaFuncao(){
+
+// }
+
+// const nomeDaFuncao2 = () =>{
+
+// }
+
+// let condicao = false
+
+// AND
+
+// se todas as expressões forem verdadeiras ele tras verdadeiro
+
+// VERDADEIRO + VERDADEIRO = VERDADEIRO
+
+// let idade = 21;
+
+
+
+// if(idade >= 18 && idade <=21){
+//     console.log("Você pode entrar");
+// }
+
+// OR
+
+// if(idade <= 15 || idade >= 21){
+
+// }
+
+
+
+// NOT
+
+// if(!idade <= 15 || idade >= 21){
+
+// }
+
+
+
+// alart
+// confirm
+// console.log
+// prompt
+
+// let nomeAuluno = "Janderson"
+// let idade = "28"
+// let altura = "42"
+// let cidade = "Fortaleza"
+// let pais = "Brasil"
+
+// alert("Aula de javascript com " + nomeAuluno + " Que tem " + idade + "anos");
+
+// alert(`Aula de javascript com o aluno ${nomeAuluno} que tem ${idade} de idade e que tem ${altura} de altura e que mora em ${cidade}`)
+
+
+// confirm("Tem certeza que dejesa executar esta ação?");
+
+// console.log(pais)
+
+// let idade = parseInt(prompt("Digite aqui sua idade"));
+
+
+// console.log(idade)
+
+// console.log(typeof(idade));
+
+
+// Faça um programa que receba do usuario o nome, o que ele estuda e a idade de acordo com o ano que nasceu, no final mostrar na tela.
+
+
+// let nomeAuluno = "Janderson"
+// let curso = "Programação"
+// let nascimento = "1979"
+// let anoatual = "2023"
+
+// alert("Aula de javascript com " + nomeAuluno + " Que cursa " + curso "e nasceu no ano de + "anoatual");
+
+// console.log(typeof(anoatual));
+
+// let nome = prompt("Digite seu nome");
+
+// let campoDeEstudo = prompt("Digite o campo de estudo");
+
+// let idade = parseInt(prompt("Digite o ano que nasceu"));
+
+// let calculoIdade = 2023 - idade;
+
+// alert(`Ola senhor ${nome} vejo que você estuda ${campoDeEstudo} e possui ${calculoIdade} anos`)
